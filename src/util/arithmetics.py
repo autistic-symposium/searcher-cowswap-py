9e8656a14c865dab83e8ebcfd2244e45d3ef1846
bt3gl <bt3@protonmail.ch>
2022-11-06 19:13:19 -0800

    one class left

--- src/util/arithmetics.py
+++ src/util/arithmetics.py
@@ -22,22 +22,22 @@ def to_decimal(value) -> Decimal:
     return Decimal(value)
 
 
-def nelder_mead_simplex_optimization(equation, boundary, initial_guess=None) -> float:
+def nelder_mead_simplex_optimization(equation, boundary_max, initial_guess=None) -> float:
     """
-        Run a simple form of the Nelder-Mead optimization solver, that 
-        minimizes a scalar function of one or more variables.
+        Run a simple form of the Nelder-Mead optimization solver,  
+        that minimizes a scalar function of one or more variables.
     """
 
-    # If initial guess (x0) is None, set to the middle
-    initial_guess = initial_guess or float(div((boundary[1] - boundary[0]), 2))
+    # If initial guess (x0) is None, set to the middle.
+    initial_guess = initial_guess or int(div(boundary_max, 2))
 
-    # Optimize to find a min of the negative of the original equation
+    # Optimize to find a min of the negative of the original equation.
     lambda_equation = lambda x: - equation(x)
 
-    # Run Nelder-Mead solver
+    # Run Nelder-Mead solver.
     solution_for_max = scipy.optimize.fmin(lambda_equation, initial_guess, disp=False)
 
     try:
-        return solution_for_max[0]
+        return int(to_decimal(solution_for_max[0]))
     except ValueError as e:
         log_error(f'Could not find a optimization solution: {e}')
